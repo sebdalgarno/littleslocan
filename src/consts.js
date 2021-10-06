5189cbd79d12b18ba4aed7e8fef387c2f7da6116
Seb Dalgarno <seb.dalgarno@gmail.com>
2021-10-05 23:27:30 -0700

    app works - transferred from little-slocan

---
+++ src/consts.js
@@ -0,0 +1,24 @@
+export const mapbox_key =
+  "pk.eyJ1Ijoibm9ydGhiZWFjaGNvbnN1bHRpbmciLCJhIjoiY2tld2ZkdXA1MmZ5cjJxcXZ4aWN5eWRteSJ9.GmGSqnuACz13roMMveTdsw";
+
+export const tileset_logging = "mapbox://northbeachconsulting.4f19d8dk";
+
+export const source_layer = ["logged_simplify", "logged_dissolve"];
+
+export const mapbox_style =
+  "mapbox://styles/northbeachconsulting/ckk9ydsvx08d917o2w9n6gnmi";
+
+// export const tileset_base = "mapbox://northbeachconsulting.0q6r8ewi";
+
+export const coordinates = [-117.7681, 49.66292];
+
+export const bounds = [
+  [-117.94154, 49.40339],
+  [-117.19460, 49.92218],
+];
+
+export const year_min = 1945;
+
+export const year_max = 2022;
+
+export const base_colors = ["#ffff14", "#d44e25"];
---
+++ src/lib/Legend.svelte
@@ -0,0 +1,68 @@
+<script>
+  export let palette;
+  export let single;
+  export let secondgrowth;
+  export let map_palette_single;
+  export let map_palette_sg;
+
+  let legend_palette = [
+    { value: "0", color: palette[0] },
+    { value: "15", color: palette[15] },
+    { value: "30", color: palette[30] },
+    { value: "45", color: palette[45] },
+    { value: ">60", color: palette[60] }
+  ];
+
+  let legend_single = [
+    { value: "Harvested", color: map_palette_single }
+  ];
+
+  let legend_sg = [
+    {value: "2nd Growth", color: map_palette_sg[1]}
+  ];
+</script>
+
+<style>
+  .legend {
+    @apply text-gray-600 p-2 z-50 text-xs;
+  }
+
+  .legendspan {
+    @apply inline-block h-2 mr-1 w-3 text-xs;
+  }
+</style>
+
+{#if single}
+  <div class="legend">
+    {#each legend_single as { value, color }}
+      <div>
+        <span class="legendspan" style="background-color: {color}" />
+        {value}
+      </div>
+    {/each}
+  </div>
+{:else}
+  <div class="legend">
+    <div class="text-xs">
+      Years Since
+      <br />
+      Harvested
+    </div>
+    {#each legend_palette as { value, color }}
+      <div>
+        <span class="legendspan" style="background-color: {color}" />
+        {value}
+      </div>
+    {/each}
+  </div>
+{/if}
+{#if secondgrowth}
+  <div class="legend -my-3">
+    {#each legend_sg as { value, color }}
+      <div>
+        <span class="legendspan" style="background-color: {color}" />
+        {value}
+      </div>
+    {/each}
+  </div>
+  {/if}

ea7f89f174576173e1ec9ac86eb85cf2079eaa67
Seb Dalgarno <seb.dalgarno@gmail.com>
2021-10-07 12:31:52 -0700

    new laer

--- src/lib/Mapbox.svelte
+++ src/lib/Mapbox.svelte
@@ -201,24 +201,13 @@
       closeOnClick: false,
     });
 
-    /////// hover popups for logging areas
     map.on("mousemove", "logged", function (e) {
-      // Change the cursor style as a UI indicator.
       map.getCanvas().style.cursor = "pointer";
 
       var coordinates = e.lngLat;
-
       var description = `
-      <p class='inline-block'>${e.features[0].properties.description}</p>`;
-      // Ensure that if the map is zoomed out such that multiple
-      // copies of the feature are visible, the popup appears
-      // over the copy being pointed to.
-      // while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-      //   coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-      // }
-
-      // Populate the popup and set its coordinates
-      // based on the feature found.
+      <p class='inline-block font-bold'> logged in ${e.features[0].properties.year}</p>`;
+      console.log(description)
       popup.setLngLat(coordinates).setHTML(description).addTo(map);
     });
 
